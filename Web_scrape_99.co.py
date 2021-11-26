838e76b2b35b806290cd146b53c850d5c9c5b2e2
hashketh <raincheck_three@hotmail.com>
2021-11-26 22:02:44 +0800

    HDB Operational
    Added Functions
    Cleaned up code
    Added gitignore file to ignore output

--- Web_scrape_99.co.py
+++ Web_scrape_99.co.py
@@ -1,8 +1,8 @@
 #! python3
-#Web_scrape_99.co.py scrapes 99.co website for predicting house prices
+# Web_scrape_99.co.py scrapes 99.co website for predicting house prices
 
 import pandas as pd
-import requests, bs4, datetime, csv
+import requests, bs4, datetime, csv, re
 
 # 99.co Robots.txt
 # User-agent: trovitBot
@@ -54,26 +54,26 @@ import requests, bs4, datetime, csv
 # Disallow: /singapore/s/sale/travel-time
 # Disallow: /singapore/new-launches/marina-one-residence
 
-#loop through each link and take all information available and store in dictionary
+# loop through each link and take all information available and store in dictionary
 
-#Intialisation of all of the lists
+# Intialisation of all of the lists
 
 NameOfProperty = ['Name_of_Property']
 Web_link = ['Web Link']
 Price = ['Price']
-PricePerSqft =['Price_per_Sqft']
+PricePerSqft = ['Price_per_Sqft']
 FloorLevel = ['Floor_Level']
 NumberOfBedrooms = ['Number_of_Bedrooms']
 Furnishing = ['Furnishing']
-Facing =['Facing']
+Facing = ['Facing']
 OverlookingView = ['Overlooking_View']
 BuiltYear = ['Built_Year']
 Tenure = ['Tenure']
-PropertyType =['Property_Type']
+PropertyType = ['Property_Type']
 Amenities = ['Amenities']
 Description = ['Description']
 DevelopmentName = ['Development_Name']
-UnitTypes =['Unit_Types']
+UnitTypes = ['Unit_Types']
 DevelopmentBuiltYear = ['Development Built Year']
 DevelopmentTenure = ['Development Tenure']
 Developer = ['Developer']
@@ -89,6 +89,9 @@ SpecialProperties = ['Closeness to MRT and Insights']
 # Landed site : https://www.99.co/singapore/sale/houses
 # EC site : https://www.99.co/singapore/sale/executive-condominiums
 
+
+# linkies
+# should the main link have an s? https?
 website_main_link = 'http://99.co'
 
 website_links = ['https://www.99.co/singapore/sale/hdb',
@@ -96,78 +99,102 @@ website_links = ['https://www.99.co/singapore/sale/hdb',
                  'https://www.99.co/singapore/sale/houses',
                  'https://www.99.co/singapore/sale/executive-condominiums']
 
-# no search terms
-
 link = website_links[0]
+first_page_bool = True
+page_counter = 2
 
-#while next_url != 'https://www.jobstreet.com.sg/en/job-search/data-scientist-jobs/1/':
 
-#for link in website_links:
-print('Searching in \n', link , '\n now')
+# 'https://www.99.co/singapore/sale/hdb?page_num=2'
 
-#individual a href buried in div class _2kH6B for HDB, Condo, Landed and EC
-res = requests.get(link)
-res.raise_for_status()
-soup = bs4.BeautifulSoup(res.text, 'html.parser')
+def write_output():
+    data = zip(*(NameOfProperty,
+                 Price,
+                 Web_link,
+                 Misc_details,
+                 Misc_details2,
+                 Amenities,
+                 SpecialProperties))
 
-# this should pick out each job in the page(listing), each page will have a maximum of 34 jobs
-results = soup.find_all('div', class_='_2kH6B')
+    file = open("99co_scrape_{}.csv".format(datetime.date.today()), 'w', newline='', encoding='utf-8')
+    wr = csv.writer(file, quoting=csv.QUOTE_ALL)
+    for item in data:
+        wr.writerow(item)
 
-print('total results ', len(results))
 
-for index, listing in enumerate(results):
-    print('--' * 30)
-    print('Entering Main Loop in Listing, index number ', index)
-    print('--' * 30,'\n')
-    if index > 10:
-        break
-    else:
-        #there should only be one link in the div class above
+def inspect_values_inlist(a_list, number):
+    print('Print first ', number, ' Results of ', a_list[0])
+    print('Length of ', a_list[0], ' is ', len(a_list))
+    for i in range(1, number + 1):
+        print(a_list[i])
+    print('\n')
+
+
+def listing_loop(link):
+    print('Entering listing function')
+    # individual a href buried in div class _2kH6B for HDB, Condo, Landed and EC
+    res = requests.get(link)
+    res.raise_for_status()
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+    # this should pick out each job in the page(listing), each page will have a maximum of 34 jobs
+    results = soup.find_all('div', class_='_2kH6B')
+    print('total results ', len(results))
+    # Main Loop
+    global first_page_bool, page_counter
+
+    for index, listing in enumerate(results):
+        print('\n','--' * 30)
+        print('Entering Main Loop in Listing, index number ', index)
+        print('--' * 30, '\n')
+
+        # The following is for stopping the running before it gets too big
+        # if index > 10:
+        #     break
+        # else:
+
+        # there should only be one link in the div class above
         result_link = listing.find('a').get('href')
-        #result_link = listing.select_one('a').get('href')
+        # result_link = listing.select_one('a').get('href')
         result_text = listing.find('a').text
-        print('Web link is ', website_main_link+result_link) # for getting link
-        print('Name of Property is ', result_text) # for getting text between the link
+        print('Web link is ', website_main_link + result_link)  # for getting link
+        print('Name of Property is ', result_text)  # for getting text between the link
         # Name can be found on listing or in the result link
         NameOfProperty.append(result_text)
-        Web_link.append(website_main_link + result_link)
+        result_link = website_main_link + result_link
+        Web_link.append(result_link)
 
-    # All of the links would have been collected in Web_link
-    # All of the names would have been collected in NameOfProperty
+        # The link would have been collected in Web_link
+        # The name would have been collected in NameOfProperty
 
-# Going into each individual link
-for index, result_link in enumerate(Web_link):
-    print('Entering Sub loop in job page')
-    # the first value of web link is the header
-    if index == 0:
-        continue
-    else:
-        print('*'*30)
-        print('Going into individual link number ', index)
+        print('\n','*' * 30)
         print('Going into individual link ', result_link)
-        print('*'*30 ,'\n')
+        print('*' * 30, '\n')
+
+        # Going into the individual link
         listing_result_link = requests.get(result_link)
         try:
             listing_result_link.raise_for_status()
         except Exception as exc:
             print('Problem due to exception ' + str(exc))
+
         listing_result_data = bs4.BeautifulSoup(listing_result_link.text, 'html.parser')
-        #listing_result_data_price = listing_result_data.select_one('div', id_='price').getText()
-        #print(listing_result_data.select_one('div', id_='price').getText())
+
+        # Price
         listing_result_data_price = listing_result_data.select_one('h2', class_='_1zGm8 _3na6W _1vzK2').getText()
         print('Price is ', listing_result_data_price)
-        listing_result_data_name = listing_result_data.select_one('h1', class_='_3Wogd JMF8h lFqTi _1vzK2').getText()
+        Price.append(listing_result_data_price)
 
-        #Getting all text data in tag 'p', class_='_2sIc2 _29qfj _2rhE-'.
-        #This includes the number of bedrooms, bathrooms, area space etc
+        # Name? Already contained from main listing
+        listing_result_data_name = listing_result_data.select_one('h1', class_='_3Wogd JMF8h lFqTi _1vzK2').getText()
+        print('Name of property is ', listing_result_data_name)
+
+        # Misc Details
+        # Getting all text data in tag 'p', class_='_2sIc2 _29qfj _2rhE-'.
+        # This includes the number of bedrooms, bathrooms, area space etc
         listing_result_data_Misc = listing_result_data.find_all('p', class_="_2sIc2 _29qfj _2rhE-")
         temp_list = [p.getText() for p in listing_result_data_Misc]
         print(temp_list)
         Misc_details.append(temp_list)
-
-        print('Price is ',listing_result_data_price)
-        print('Name of property is ', listing_result_data_name)
-        Price.append(listing_result_data_price)
 
         # Everything in Property Details and Development Overview
         listing_result_data_Misc2 = listing_result_data.find_all('div', class_='_2dry3')
@@ -187,55 +214,58 @@ for index, result_link in enumerate(Web_link):
         print('MRT ', temp_list4)
         SpecialProperties.append(temp_list4)
 
+    # End of Job Listing
 
-def inspect_values_inlist(a_list, number):
-    print('Print first ', number ,' Results of ', a_list[0])
-    print('Length of ',a_list[0], ' is ', len(a_list))
-    for i in range(1,number+1):
-        print(a_list[i])
-    print('\n')
+# for link in website_links:
+
+
+# Start of Main Body
+# Checks if it is the first page? -> Go run listing loop function
+# if not first page -> Checks page counter
+# if page counter < specified number, fetch next link and run listing loop function
+
+def __main__():
+    global page_counter, hdb_next_link
+    print('First page bool status is ', first_page_bool)
+    print('Page counter is ', page_counter)
+    print('Searching in \n', link, '\n now')
+
+    listing_loop(link)
+    # if this is the first page then it will set it to false
+    # then once its not the first page it will increase
+    # i.e First encounter sets to false, second encounter, ups the page counter
+
+    print('First page bool status is ', first_page_bool)
+    print('Page counter is ', page_counter)
+
+    while page_counter <228 :
+
+        hdb_next_link = website_main_link + '/singapore/sale/hdb?page_num=' + str(page_counter)
+        print('*-' * 30)
+        print('Attempting next link ', hdb_next_link)
+        print('*-' * 30, '\n')
+        listing_loop(hdb_next_link)
+        page_counter += 1
+
+# End of one category
+# reset to first page status
+# first_page_bool = True
+# page_counter = 2
+
+__main__()
+
+print('\n', '*-' * 30)
+print('All Done')
+print('*-' * 30, '\n')
 
 # Inspect first 5 values....
 inspect_values_inlist(NameOfProperty, 5)
 inspect_values_inlist(Price, 5)
 inspect_values_inlist(Web_link, 5)
-inspect_values_inlist(Misc_details,5)
-inspect_values_inlist(Misc_details2,5)
-inspect_values_inlist(Amenities,5)
-inspect_values_inlist(SpecialProperties,5)
+inspect_values_inlist(Misc_details, 5)
+inspect_values_inlist(Misc_details2, 5)
+inspect_values_inlist(Amenities, 5)
+inspect_values_inlist(SpecialProperties, 5)
 
-# Expected output should be 34. There are promoted listings that are not captured which is good
-
-#print('Number of results is ', str(len(result)))
-#print('Printing searches of result ', result[0])
-
-# df = pd.DataFrame(columns = [NameOfProperty,
-#                   Price,
-#                   Web_link,
-#                   Misc_details,
-#                   Misc_details2,
-#                   Amenities,
-#                   SpecialProperties])
-
-data = zip(*(NameOfProperty,
-            Price,
-            Web_link,
-            Misc_details,
-            Misc_details2,
-            Amenities,
-            SpecialProperties))
-
-file = open("99co_scrape_{}.csv".format(datetime.date.today()), 'w', newline='', encoding='utf-8')
-wr = csv.writer(file, quoting=csv.QUOTE_ALL)
-for item in data:
-    wr.writerow(item)
-
-# df = pd.DataFrame(data)
-# df_T = df.transpose()
-#
-# print(df.head())
-#
-# print(df_T.head())
-#
-# df.to_csv('99co_scrape_{}.csv'.format(datetime.date.today()),
-#           index = False)
+# Outputs to csv
+write_output()
