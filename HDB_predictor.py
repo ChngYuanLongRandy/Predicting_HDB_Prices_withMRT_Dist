919a8b709bde72c61d42c688551789210901c8a2
hashketh <raincheck_three@hotmail.com>
2021-12-06 16:24:52 +0800

    more files

---
+++ HDB_predictor.py
@@ -0,0 +1,16 @@
+#! Python3
+# Runs the script on streamlit
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+df = 
+
+st.title("Predicting HDB Prices")
+
+st.write('''
+Select House Details below:
+''')
+
+st_address = st.selectbox("Select Address", )
